25ee5338d5b3ffd31ad39c7c897b49a31bd2e5a6
phantom08266 <phantom08266@gmail.com>
2019-07-01 22:25:07 +0900

    9095문제 다시 재귀함수로 구현하여 풀어봄.

--- Alogo/Permutation/ex9095.h
+++ Alogo/Permutation/ex9095.h
@@ -27,21 +27,42 @@ namespace ex9095
 		}
 		return count;
 	}
+	//n은 목표값
+	int SumSameValue2(int n, int sum)
+	{
+		int count = 0;
+		if (n < sum) // 불가능 경우의 수
+		{
+			return 0;
+		}
+		if (n == sum) // 탈출조건
+		{
+			return 1;
+		}
+		for (int i = 1; i <= 3; i++) // 다음 경우의 수
+		{
+			count += SumSameValue2(n, sum + i);
+		}
+		return count;
+	}
 	void ex9095_Run()
 	{
 		int loop;
 		int count = 0;
-		
+
 		cin >> loop;
 
 		int n;
 		for (int i = 0; i < loop; i++)
 		{
 			cin >> n;
+			TimeStart();
 			count = 0;
-			count = SumSameValue(n, 10, 0);
+			//count = SumSameValue(n, 10, 0);
+			count = SumSameValue2(n, 0);
 			cout << count << endl;
+			TimePrint();
 		}
-		
+
 	}
 }
